27bc9afe3d0f6608fe0a7acaf13bab24aef6c4b0
MadsSK <Mads.saustrup.kristensen@gmail.com>
2018-03-19 09:45:03 +0100

    translated wind direction

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,7 +1,68 @@
+var request = require("request");
+
 module.exports = function(app){
   
+  var data = {
+    city : "Copenhagen",
+    temp: "",
+    humidity: "",
+    wind: "",
+    heading: "",
+    status: ""
+  }
+
+  var apiRequest = {
+    url: "https://api.openweathermap.org/data/2.5/weather",
+    qs: {
+      q: "Copenhagen",
+      units: "metric",
+      appid: "166d00e26d3ff2c6149e89feccc5c59a"
+    }  
+  };
+
   //respone to GET request
   app.get("/", function(req, res){
-    res.render('weather');
+    if(req.query.city==undefined){
+      apiRequest.qs.q = "Copenhagen";
+    }else{
+      apiRequest.qs.q = req.query.city;
+    }
+
+    request.get(apiRequest, function(error, response, body){
+      if(!error && response.statusCode == 200){
+        var JSONbody = JSON.parse(body);
+        data.city = JSONbody.name;
+        data.temp = JSONbody.main.temp;
+        data.humidity = JSONbody.main.humidity;
+        data.wind = JSONbody.wind.speed;
+        data.heading = degreesToDanishCompassheadings(JSONbody.wind.deg);        
+        data.status = "is-valid";
+      }else{
+        data.status = "is-invalid";
+      }
+      res.render('weather', {data: data});
+    });
+
+    
   });
 }
+
+var degreesToDanishCompassheadings = function(degrees){
+  if(degrees >= 337,5 || degrees < 22,5){
+    return "Nord";
+  }else if(degrees >= 22,5 && degrees < 45+22,5){
+    return "Nordøst";
+  }else if(degrees >= 45+22,5 && degrees < 2*45+22,5){
+    return "Øst";
+  }else if(degrees >= 2*45+22,5 && degrees < 3*45+22,5){
+    return "Sydøst";
+  }else if(degrees >= 3*45+22,5 && degrees < 4*45+22,5){
+    return "Syd";
+  }else if(degrees >= 4*45+22,5 && degrees < 5*45+22,5){
+    return "Sydvest";
+  }else if(degrees >= 5*45+22,5 && degrees < 6*45+22,5){
+    return "Vest";
+  }else if(degrees >= 6*45+22,5 && degrees < 7*45+22,5){
+    return "NordVest";
+  }
+};
